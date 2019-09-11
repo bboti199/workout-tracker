9106a627432d81dd432b7657fccb41d3b87c2ec0
bboti199 <b.boti199@yahoo.com>
2019-09-11 20:42:01 +0300

    ..

--- client/src/redux/reducers/alert.js
+++ client/src/redux/reducers/alert.js
@@ -1,12 +1,12 @@
 import { SET_ALERT, REMOVE_ALERT } from '../actions/types';
 
-const initialState = {};
+const initialState = { errors: [] };
 
 export default function(state = initialState, action) {
   const { type, payload } = action;
   switch (type) {
     case SET_ALERT:
-      return [...state, payload];
+      return [payload];
     case REMOVE_ALERT:
       return {};
     default:
